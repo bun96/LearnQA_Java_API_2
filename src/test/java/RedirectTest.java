f0219b002964c6313756c876b826a577ec2b8e6c
Mikhail Lyubeznov <trudqwert123@gmail.com>
2024-02-11 09:28:38 +0300

    redirect_test

---
+++ src/test/java/RedirectTest.java
@@ -0,0 +1,22 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+
+public class RedirectTest {
+    @Test
+    public void redirectTest () {
+
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+        response.prettyPrint();
+
+        String redirectAdress = response.getHeader("Location");
+        System.out.println(redirectAdress);
+    }
+}
